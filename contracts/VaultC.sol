29dc6bbe505f3606c01bef0f7870b236caeed0a9
Abhijit Roy <dalvath3700@gmail.com>
2021-10-23 12:43:17 +0530

    code is bettered
    - msgSender() from Context.sol OpenZeppelin replaced 'msg.sender'
    - removed emitting event when transaction reverts

--- contracts/VaultC.sol
+++ contracts/VaultC.sol
@@ -6,6 +6,7 @@ import '@openzeppelin/contracts/access/Ownable.sol';
 import '@openzeppelin/contracts/security/Pausable.sol';
 import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import '@openzeppelin/contracts/utils/Context.sol';
 import './interfaces/ICoin.sol';
 import "hardhat/console.sol";
 
@@ -32,7 +33,6 @@ contract VaultC is Ownable, Pausable, ReentrancyGuard {
 
     // ==========Events=============================================
     event Deposited(address indexed depositor, uint256 collateralDeposited, uint256 amountMinted);
-    event DepositMintDebtAmtFailed(address indexed depositor, uint256 collateralDeposited, uint256 amountMinted);
     event Withdrawn(address indexed withdrawer, uint256 collateralWithdrawn, uint256 amountBurned);
     event WithdrawTransferETHFailed(address indexed withdrawer, uint256 collateralWithdrawn, uint256 amountBurned);
     event Received(address, uint);
@@ -54,36 +54,31 @@ contract VaultC is Ownable, Pausable, ReentrancyGuard {
      * @dev receive ETH from any address via low-level interaction.
      */     
     receive() external payable {
-        emit Received(msg.sender, msg.value);
+        emit Received(_msgSender(), msg.value);
     }
     
     // -------------------------------------------------------------
     /// @notice Allows a user to deposit ETH collateral in exchange for some amount of stablecoin
-    /// @param amountToDeposit deposit the amount of ether the user sent in the transaction. Unit: wei
-    // function deposit() external payable whenNotPaused nonReentrant {
-    function deposit(uint256 amountToDeposit) external payable whenNotPaused nonReentrant {
-        require(msg.sender != address(0));
-        require( msg.value > 0 && msg.value == amountToDeposit, "amount transferred must be positive and equal to parsed amount");
+    /// @dev deposit the amount of ether the user sent in the transaction. Unit: wei
+    function deposit() external payable whenNotPaused nonReentrant {
+        require(_msgSender() != address(0));
+        require( msg.value > 0, "amount transferred must be positive and equal to parsed amount");
 
         // debtAmount calc
         // NOTE: assumption has been made
-        uint256 nda = _getTokenAmount(msg.value);        // new debt amount calculated in wei
+        uint256 newDebtAmt = _getTokenAmount(msg.value);        // new debt amount calculated in wei
 
-        uint256 ca = vaultBalances[msg.sender].collateralAmount;     // stored collateral amount
-        uint256 da = vaultBalances[msg.sender].debtAmount;           // stored debt amount
+        uint256 colAmt = vaultBalances[_msgSender()].collateralAmount;     // stored collateral amount
+        uint256 debtAmt = vaultBalances[_msgSender()].debtAmount;           // stored debt amount
 
         // update the collateral, debt amounts
-        vaultBalances[msg.sender].collateralAmount = ca.add(msg.value);       // add in wei
-        vaultBalances[msg.sender].debtAmount = da.add(nda);                   // add in wei
+        vaultBalances[_msgSender()].collateralAmount = colAmt.add(msg.value);       // add in wei
+        vaultBalances[_msgSender()].debtAmount = debtAmt.add(newDebtAmt);                   // add in wei
 
         // mint debt amount corresponding to deposited ETH
-        bool success = token.mint(msg.sender, nda);               // parse in wei
-        if(success) {
-            emit Deposited(msg.sender, msg.value, nda);                   // show in Wei for events    
-        } else {
-            emit DepositMintDebtAmtFailed(msg.sender, msg.value, nda);    // show in Wei for events        
-            revert("Token Minting of debt amount failed during deposit.");
-        }
+        bool success = token.mint(_msgSender(), newDebtAmt);               // parse in wei
+        require(success, "Token Minting of debt amount failed during deposit.");
+        emit Deposited(_msgSender(), msg.value, newDebtAmt);                   // show in Wei for events    
     }
 
     // -------------------------------------------------------------
@@ -91,34 +86,30 @@ contract VaultC is Ownable, Pausable, ReentrancyGuard {
     /// @dev This cannot allow a user to withdraw more than they put in
     /// @param repaymentAmount  the amount of stablecoin that a user is repaying to redeem their collateral for. Unit: Wei
     function withdraw(uint256 repaymentAmount) external whenNotPaused nonReentrant {
-        require(msg.sender != address(0));
-        require(repaymentAmount <= token.balanceOf(msg.sender), "Insufficient AUDC balance");
+        require(_msgSender() != address(0));
+        require(repaymentAmount <= token.balanceOf(_msgSender()), "Insufficient AUDC balance");
 
         // read amounts
-        uint256 ca = vaultBalances[msg.sender].collateralAmount;     // stored collateral amount in Wei
-        uint256 da = vaultBalances[msg.sender].debtAmount;           // stored debt amount in Wei
+        uint256 colAmt = vaultBalances[_msgSender()].collateralAmount;     // stored collateral amount in Wei
+        uint256 debtAmt = vaultBalances[_msgSender()].debtAmount;           // stored debt amount in Wei
 
-        require(repaymentAmount <= da, "repayment amount must be less than stored debt amount");
+        require(repaymentAmount <= debtAmt, "repayment amount must be less than stored debt amount");
 
         // calc ETH in Wei for repayment amount
         uint256 transferAmt = _getWeiAmount(repaymentAmount);
 
         // update the collateral, debt amounts
-        vaultBalances[msg.sender].collateralAmount = ca.sub(transferAmt);
-        vaultBalances[msg.sender].debtAmount = da.sub(repaymentAmount);
+        vaultBalances[_msgSender()].collateralAmount = colAmt.sub(transferAmt);
+        vaultBalances[_msgSender()].debtAmount = debtAmt.sub(repaymentAmount);
 
         // burn repayment tokens
-        bool success = token.burn(msg.sender, repaymentAmount);
+        bool success = token.burn(_msgSender(), repaymentAmount);
         require(success, "Token burning of repayment tokens failed during withdraw");
 
         // transfer ETH (in wei) corresponding to repayment amount
-        (bool sent, /*bytes memory data*/) = msg.sender.call{value: transferAmt}("");     // parse in wei
-        if(sent) {
-            emit Withdrawn(msg.sender, transferAmt, repaymentAmount);               // show in Wei for events
-        } else {
-            emit WithdrawTransferETHFailed(msg.sender, transferAmt, repaymentAmount);       // show in Wei for events
-            revert("ETH Token Transfer failed during withdraw.");
-        }
+        (bool sent, /*bytes memory data*/) = _msgSender().call{value: transferAmt}("");     // parse in wei
+        require(sent, "ETH Token Transfer failed during withdraw.");
+        emit Withdrawn(_msgSender(), transferAmt, repaymentAmount);               // show in Wei for events
     }
 
     // -------------------------------------------------------------
